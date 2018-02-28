fe5082eade636568b7874a019b30f557cad82a66
David Hirvonen <dhirvonen@elucideye.com>
2018-02-28 00:24:08 -0300

    include ES2/glext2.h when using ES3

--- ogles_gpgpu/platform/opengl/gl_includes.h
+++ ogles_gpgpu/platform/opengl/gl_includes.h
@@ -28,6 +28,7 @@
 #    if defined(OGLES_GPGPU_OPENGL_ES3)
 #      include <OpenGLES/ES3/gl.h>
 #      include <OpenGLES/ES3/glext.h>
+#      include <OpenGLES/ES2/glext2.h>
 #    else
 #      include <OpenGLES/ES2/gl.h>
 #      include <OpenGLES/ES2/glext.h>
